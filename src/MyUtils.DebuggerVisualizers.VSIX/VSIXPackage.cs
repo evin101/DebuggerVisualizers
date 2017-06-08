86607b393871bb4673f834695458921750eb2fee
evin101 <evin101@gmail.com>
2017-06-08 21:53:57 +0900

    fix VSIX

--- src/MyUtils.DebuggerVisualizers.VSIX/VSIXPackage.cs
+++ src/MyUtils.DebuggerVisualizers.VSIX/VSIXPackage.cs
@@ -7,22 +7,23 @@ using System.Diagnostics;
 
 namespace MyUtils.DebuggerVisualizers.VSIX
 {
-   [PackageRegistration(UseManagedResourcesOnly = true)]
-   [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-   [Guid(VSIXPackage.PackageGuidString)]
-   [ProvideAutoLoad(Microsoft.VisualStudio.VSConstants.UICONTEXT.NoSolution_string)]
-   public sealed class VSIXPackage : Package
-   {
-      public const string PackageGuidString = "e9737e5e-4cbf-4c0c-91d7-d40f96839286";
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid(VSIXPackage.PackageGuidString)]
+    [ProvideAutoLoad(Microsoft.VisualStudio.VSConstants.UICONTEXT.NoSolution_string)]
+    public sealed class VSIXPackage : Package
+    {
+        public const string PackageGuidString = "e9737e5e-4cbf-4c0c-91d7-d40f96839286";
 
-      public VSIXPackage()
-      {
-      }
+        public VSIXPackage()
+        {
+        }
 
         // This method is called when VS loads because this package is marked with ProvideAutoLoad attribute
         protected override void Initialize()
         {
             const string PAYLOAD_FILE_NAME = "MyUtils.DebuggerVisualizers.dll";
+            const string SATELLITE_ASSEMBLIES = "MyUtils.DebuggerVisualizers.*.dll";
 
             string sourceFolderFullName;
             string destinationFolderFullName;
@@ -49,11 +50,8 @@ namespace MyUtils.DebuggerVisualizers.VSIX
                 sourceFileFullName = Path.Combine(sourceFolderFullName, PAYLOAD_FILE_NAME);
                 destinationFileFullName = Path.Combine(destinationFolderFullName, PAYLOAD_FILE_NAME);
 
-#if DEBUG
-                File.Copy(sourceFileFullName, destinationFileFullName, true);
-#else
-                CopyFileIfNewerVersion(sourceFileFullName, destinationFileFullName);
-#endif
+
+                CopyFilesIfNewerVersion(sourceFolderFullName, destinationFolderFullName, PAYLOAD_FILE_NAME, SATELLITE_ASSEMBLIES);
 
             }
             catch (Exception ex)
@@ -63,36 +61,53 @@ namespace MyUtils.DebuggerVisualizers.VSIX
             }
         }
 
-      private void CopyFileIfNewerVersion(string sourceFileFullName, string destinationFileFullName)
-      {
-         FileVersionInfo destinationFileVersionInfo;
-         FileVersionInfo sourceFileVersionInfo;
-         bool copy = false;
+        private void CopyFilesIfNewerVersion(string sourceDir, string destinationDir, string targetAssembly, string satelliteAssemblies)
+        {
 
-         if (File.Exists(destinationFileFullName))
-         {
-            sourceFileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(sourceFileFullName);
-            destinationFileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(destinationFileFullName);
-            if (sourceFileVersionInfo.FileMajorPart > destinationFileVersionInfo.FileMajorPart)
-            {
-               copy = true;
-            }
-            else if (sourceFileVersionInfo.FileMajorPart == destinationFileVersionInfo.FileMajorPart 
-               && sourceFileVersionInfo.FileMinorPart > destinationFileVersionInfo.FileMinorPart)
-            {
-               copy = true;
-            }
-         }
-         else
-         {
-            // First time
-            copy = true;
-         }
+            string sourceFileFullName = Path.Combine(sourceDir, targetAssembly);
+            string destinationFileFullName = Path.Combine(destinationDir, targetAssembly);
 
-         if (copy)
-         {
+            if (File.Exists(destinationFileFullName))
+            {
+                FileVersionInfo sourceFileVersionInfo = FileVersionInfo.GetVersionInfo(sourceFileFullName);
+                FileVersionInfo destinationFileVersionInfo = FileVersionInfo.GetVersionInfo(destinationFileFullName);
+
+                if (!IsNewerVersion(sourceFileVersionInfo, destinationFileVersionInfo))
+                {
+                    return;
+                }
+            }
+
             File.Copy(sourceFileFullName, destinationFileFullName, true);
-         }
-      }
-   }
+            foreach (var fileFullPath in Directory.GetFiles(sourceDir, satelliteAssemblies, SearchOption.AllDirectories))
+            {
+                FileInfo sourceFileInfo = new FileInfo(fileFullPath);
+                FileInfo destinationFileInfo = new FileInfo(Path.Combine(destinationDir, fileFullPath.Replace(sourceDir, "").TrimStart('\\')));
+
+
+                if (!destinationFileInfo.Directory.Exists)
+                {
+                    destinationFileInfo.Directory.Create();
+                }
+                sourceFileInfo.CopyTo(destinationFileInfo.FullName, true);
+            }
+        }
+
+        private bool IsNewerVersion(FileVersionInfo source, FileVersionInfo destination)
+        {
+            if (source.FileMajorPart > destination.FileMajorPart)
+            {
+                return true;
+            }
+            else if (source.FileMajorPart == destination.FileMajorPart
+               && source.FileMinorPart > destination.FileMinorPart)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
 }
